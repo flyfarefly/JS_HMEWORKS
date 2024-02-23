473335f5e283bca8adce9149c243744772b5b792
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-02-23 17:07:13 +0200

    Homework #7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 `use strict`;
+
+// Task #1
+
+let admin, name;
+name = "John";
+admin = name;
+alert( admin ); // "John"
+
+// Task #2
+
+const planet = "Earth";
+let userName;
+
+// Task #3
+
+let number;
+number = "10";
+console.log(typeof number);
+
+number = 10;
+console.log(typeof number);
