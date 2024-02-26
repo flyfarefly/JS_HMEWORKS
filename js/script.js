890fe7de50d8e5596add7951fed9600d515a1049
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-02-27 00:29:57 +0200

    Homework #5 Hello User

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 `use strict`;
+
+
+// Task#
+const test = confirm("Згенерувати випадкове п'ятизначне число");
+console.log(test);
+if ( test === true) {
+    alert("Трохи почекайте!");
+    function randomInt(min,max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+const num = (randomInt(10000,99999));
+alert (num);
+console.log(typeof num);
+
+let arr = String(num).split('').join(` `);
+alert (arr);
+console.log(typeof arr);
+}
