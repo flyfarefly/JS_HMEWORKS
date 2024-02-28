d995f9fff82c9f4609c6b67cca8abfb5f75d98f4
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-02-28 16:12:20 +0200

    HW_8 Age

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 `use strict`;
+
+const age = Number(prompt(`Скільки років тобі?`));
+if (isNaN(age)) {
+    alert('Напишіть число, будь ласка!');
+} else {
+    alert(`А через десять років тобі буде ${age + 10}`);
+}
+
+
+
+
+
+
