8e9fc051d18344fe71a7bd5347ca66a92753c3e5
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-03 17:22:39 +0200

    HW_12 Objects all task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 `use strict`;
+
+// Task#1
+
+// let user = new Object();
+
+// const user = {};
+// user.name = 'John';
+// user.surname = 'Smith';
+// console.log(user);
+//
+// user.name = 'Pete';
+// console.log(user);
+//
+// delete user.name;
+// console.log(user);
+
+//Task#2
+
+// const user = {
+//     name: 'John'
+// };
+
+// Чи буде це працювати та чому ?
+
+// user.name = 'Pete'; // Це властивість об'єкту. Вона може змінюватись.
+// console.log(user);
+
+// Task#3
+
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+
+};
+sum = salaries.John+ salaries.Ann+salaries.Pete;
+console.log(sum);
+console.log(typeof sum);
