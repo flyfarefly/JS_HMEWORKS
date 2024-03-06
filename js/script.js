232f054da3bc815151f5b6ae49c3177ebf12289a
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-06 20:29:44 +0200

    HW_14 Math operations short

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 `use strict`;
+
+const num1 = Number(prompt(`Введіть перше число`));
+const num2 = Number(prompt(`Введіть друге число`));
+
+alert(`// Користувач ввів ${num1} та ${num2}:\n
+* ${num1} + ${num2} = ${num1 + num2}\n
+* ${num1} - ${num2} = ${num1 - num2}\n
+* ${num1} * ${num2} = ${num1 * num2}\n
+* ${num1} / ${num2} = ${num1 / num2}`);
