1cc46d88986f1029c207fb12da2d33e87efcb5ef
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-14 15:33:08 +0200

    HW_26 Labels in cycles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 `use strict`;
+let sum = 0;
+for (let i = 0; i <= 20 ; i++) {
+         if (i % 2 === 0) continue;
+         sum += i;
+    console.log(i, sum);
+}
+console.log(`Сума непарних чисел від 1 до 20: ${sum}`);
+
+
+
