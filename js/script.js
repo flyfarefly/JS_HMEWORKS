73daef00a1bc7dfb5f231876554402e07349eda4
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-14 15:56:03 +0200

    HW_24 Loops and arrays

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 `use strict`;
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+let multi = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+for (let i = 0; i < arr.length; i++) {
+    multi += arr[i]** 2;
+}
+
+console.log(`Сума елементів масиву: ${sum}`);
+console.log(`Сума квадратів елементів масиву: ${multi}`);
