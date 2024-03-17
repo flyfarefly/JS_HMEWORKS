ad83fea4c437cf6c9a53480cafc4f2c82bc4ae66
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-17 23:25:13 +0200

    HW_30 Functions V3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 `use strict`;
+// const arr = [1, 2, 3, -1, -2, -3];
+//
+//
+// const findSmallestDivisor = function (number) {
+// for (let i = 10; i <= 20; i++) {
+//
+//     const exampleArr = []; // Пустий масив для зберігання чисел
+//
+//     exampleArr.push(i); // Додаємо число до масиву
+//
+// }
 
 
-// Task#1.2
-let numbers = []; // Пустий масив для зберігання чисел
-for (let i = 10; i <= 20; i++) {
-    numbers.push(i); // Додаємо число до масиву
-}
-console.log(numbers); // Виведе [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-let numbersString = numbers.join(', ');
-console.log(numbersString); // Виведе "10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20"
+
 
