2e94272936900a926778f2612078edaedb912de4
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-17 21:15:10 +0200

    HW_25 Minimum divisor

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 `use strict`;
-const n = + prompt(`Введіть число`);
-let k = 0; // Кількість дільників
-let i = 0; // Дільники
-let s = 0; // Сума дільників
+const myNumber = +prompt(`Введіть число`);
+const findSmallestDivisor =  function (number) {
+    if (number === 1) {
+        return 1; // Якщо число менше або дорівнює 1, то мінімальний дільник - 1
+    }
+    if (number < 1) {
+        return NaN; // Якщо число менше ніж 1 виводимо NaN
+    }
 
-if (Number.isInteger(n)) {
-    for (; i <= n; i++) {
-        if (n % i < 1) {
-            console.log (`Nan`);
-
-            if (n % i === 0) {
-                console.log (`Nan`);
-
-            }else {
-                console.log (i)
-            }
+    for (let divisor = 2; divisor <= number; divisor++) {
+        if (number % divisor === 0) {
+            return divisor; // Знайдено мінімальний дільник
         }
     }
-    console.log(`Парних дільників ${k}, а їх сума дорівнює ${s}`);
+    return number; // Якщо не знайдено інших дільників, то саме число є мінімальним дільником
 }
+
+const smallestDivisor = findSmallestDivisor(myNumber);
+console.log(`Мінімальний дільник числа ${myNumber} - це ${smallestDivisor}`);
