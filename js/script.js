995aa8e183e0c7d4c27902f6e0f0a2896e57a7ec
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-17 23:59:18 +0200

    HW_27 Implement the generateKey function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 `use strict`;
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const keyLength = 16;
+
+const generateKey = function (length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+
+        result += characters[randomIndex];
+
+    }
+    return result;
+}
+
+const generatedKey = generateKey(keyLength, characters);
+console.log(`Згенерований ключ: ${generatedKey}`);
