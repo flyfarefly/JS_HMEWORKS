3910310c404d564f8a53663b0115970992404842
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-17 14:52:04 +0200

    HW_25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 `use strict`;
+const n = + prompt(`Введіть число`);
+let k = 0; // Кількість дільників
+let i = 0; // Дільники
+let s = 0; // Сума дільників
+
+if (Number.isInteger(n)) {
+    for (; i <= n; i++) {
+        if (n % i < 1) {
+            console.log (`Nan`);
+
+            if (n % i === 0) {
+                console.log (`Nan`);
+
+            }else {
+                console.log (i)
+            }
+        }
+    }
+    console.log(`Парних дільників ${k}, а їх сума дорівнює ${s}`);
+}
