024a2e594a5bb12e345d8efdd65f06779240a92e
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-24 15:04:18 +0200

    HW_32 Array Methods V2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 `use strict`;
+
+
+//Task#9
+const reverse = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const currentElement = arr[i];
+
+        // const lastElement = arr[arr.length - (1 + i)];
+        // arr[i] = lastElement;
+
+        arr [i] = arr[arr.length - (1 + i)];
+        arr[arr.length - (1 + i)] = currentElement;
+
+        if (i + 1 === Math.floor(arr.length / 2)) break;
+    }
+    return arr;
+}
+console.log(reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
+console.log(reverse([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,12,13]))
