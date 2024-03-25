7269c9ff696df19894ebf8bfaec4d9ec6562f367
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-03-25 10:02:45 +0200

    HW_31 Array method V1 full

--- js/script.js
+++ js/script.js
@@ -29,13 +29,39 @@ console.log(`Перший елемент масиву: ${arr2[0]}`);
 const arr3 = ['js', 'css', 'jq'];
 console.log(`Останній елемент масиву: ${arr3[arr3.length - 1]}`);
 
-
 // Task#07
+const dadArray = [1, 2, 3, 4, 5];
+const newArray = dadArray.slice(0, 3);
+console.log(`Новий масив з елементами: ${newArray}`);
+
+
 // Task#08
+const originalArray = [1, 2, 3, 4, 5];
+const cutArray = originalArray.splice(3, 2);
+console.log(`Новий масив з елементами: ${cutArray}`);
+
 // Task#09
+const momArray = [1, 2, 3, 4, 5];
+const sliceArray = momArray.slice(1, 3);
+console.log(`${sliceArray}`);
+
 // Task#10
+const familyArray = [1, 2, 3, 4, 5];
+const childArray = familyArray.slice(1, 4);
+console.log(`${childArray}`);
+
 // Task#11
+const friendsArray = [1, 2, 3, 4, 5];
+friendsArray.splice(1, 3);
+friendsArray.push('a', 'b', 2, 'c', 5, 'e');
+console.log(`Оновлений масив: ${friendsArray}`);
+
 // Task#12
+const badArray = [1, 2, 3, 4, 5];
+const goodArray = badArray.splice(3, 2);
+badArray.push('a', 'b', 'c');
+const maybeArray = badArray.concat(goodArray);
+console.log(`Оновлений масив: ${maybeArray}`);
 
 // Task#13
 const arrSort = [3, 4, 1, 2, 7];
@@ -46,11 +72,23 @@ console.log(`Відсортований масив: ${arrSort}`);
 
 const arr4 = [5, 6, 7, 8, 9];
 let sum = 0;
-
 for (let i = 0; i < arr4.length; i++) {
     sum += arr4[i];
 }
 console.log(`Сума елементів масиву: ${sum}`);
+
+// Task14.1
+let sum2 = arr4.reduce(function (acc, item) {
+    return acc + item;
+}, 0)
+console.log(`Сума елементів масиву: ${sum2}`);
+
+// Task14.2
+let sum3 = arr4.reduceRight(function (acc, item) {
+    return acc + item;
+}, 0)
+console.log(`Сума елементів масиву: ${sum3}`);
+
 
 // Task#15
 const arr5 = [5, 6, 7, 8, 9];
@@ -76,16 +114,11 @@ const newString = arr8.filter((item) => item.length > 5);
 console.log(`Новий масив з строк ${newString} з масиву  ${arr8}`);
 
 // Task#19
-// const arr9 =  [1, 2, [3, 4], 5, [6, 7]];
-
+const arr9 = [1, 2, [3, 4], 5, [6, 7]];
+const arrayOnly = arr9.filter(element => Array.isArray(element));
+console.log(`${arrayOnly}`);
 
 // Task#20
-const arr10 =  [5,-3, 6,-5, 0,-7, 8, 9];
+const arr10 = [5, -3, 6, -5, 0, -7, 8, 9];
 const negativeNumbers = arr10.filter((item) => item < 0);
-console.log(`В масиві ${arr10} негативних чисел  ${negativeNumbers.length} штук`);
-
-
-
-
-
-
+console.log(`В масиві ${arr10} негативних чисел ${negativeNumbers.length} штук`);
