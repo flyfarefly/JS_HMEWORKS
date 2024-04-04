26bc25bbed80c38eba932d90ab05cfc865c9269a
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-04-04 12:43:44 +0300

    HW_28 Function exponentiation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 `use strict`;
+const power = function (base, exponent = 1) {
+    if (typeof base !== 'number' || typeof exponent !== 'number') {
+        return 'Помилка: введено неправильні дані';
+    }
+    const result = Math.pow(base, exponent);
+    return result;
+}
 
-
+const baseNumber = parseFloat(prompt('Введіть число:'));
+const exponentNumber = parseFloat(prompt('Введіть ступінь:'));
+const resultValue = power(baseNumber, exponentNumber);
+alert(`Результат: ${resultValue}`);
