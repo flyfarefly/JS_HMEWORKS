c54d63c2c20f29339a5bb323bb22785b121bd8ca
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-04-19 15:35:54 +0300

    HW_52 Recursion V1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 `use strict`;
+// Task #1
+const getFactorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * getFactorial(n - 1);
+};
+
+const number = 4;
+console.log(`${number}! = ${getFactorial(number)}`);
+
+// Task #2
+const pow = (x, n) => {
+  if (n === 1) {
+    return x;
+  }
+  return x * pow(x, n - 1);
+};
+
+const x = 3;
+const n = 6;
+console.log(
+  `При возведенні числа ${x} в ${n} ступінь отримаємо число ${pow(x, n)}.`,
+);
+
+// Task #3
+const recursiveSum = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return recursiveSum(a + 1, b - 1);
+};
+
+const a = 3;
+const b = 6;
+
+console.log(`Сума ${a} і ${b} дорівнює ${recursiveSum(a, b)}.`);
