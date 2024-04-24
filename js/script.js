d91b90ab0587528a498ae9157dcb1a2024cfea65
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-04-24 21:35:59 +0300

    V1 closures done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+// eslint-disable-next-line no-unused-expressions,quotes
 `use strict`;
+// eslint-disable-next-line func-names
+(function () {
+  function createSumFunction() {
+    let total = 0;
+
+    function sum(n) {
+      total += n;
+      return total;
+    }
+
+    // eslint-disable-next-line func-names
+    sum.valueOf = function () {
+      return total;
+    };
+
+    return sum;
+  }
+
+  const sum = createSumFunction();
+
+  console.log(sum(3)); // 3
+  console.log(sum(5)); // 8
+  console.log(sum(20)); // 28
+})();
