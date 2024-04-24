49c803ad8c054d2066a8cc6e622610bc3df3c0fa
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-04-24 12:31:22 +0300

    rules eslint/ readme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// eslint-disable-next-line no-unused-expressions
 `use strict`;
+
+const arr = [1, 2, 3, 4, [5, 6, 7, 8]];
+// const flattened = arr.flat();
+// console.log(flattened);
+
+const flatten = ([arr]) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    return arr.push[i];
+  }
+  return result;
+};
+
+console.log(flatten(arr));
