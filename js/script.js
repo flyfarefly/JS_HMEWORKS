3067c970a95cfbdf3fa4cf695211b1a64004ff2b
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-04-25 19:14:20 +0300

    HW_54 Bind #first

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-`use strict`;
+(function () {
+  // eslint-disable-next-line no-unused-expressions
+  `use strict`;
+  const user = {
+    firstName: "Vova",
+    lastName: "Daniel",
+    age: 5,
+    foo() {
+      console.log(this.lastName);
+    },
+  };
+  // user.foo();
+  const call = function (func, ctx, arrayOfArgs) {
+    ctx.func = func;
+    ctx.func(arrayOfArgs);
+    delete ctx.func;
+  };
+  const bind = function (func, ctx, arrayOfArgs) {
+    return function () {
+      call(func, ctx, arrayOfArgs);
+    };
+  };
+  const display = function () {
+    console.log(this.lastName);
+  };
+  call(display, user);
+  const d = bind(display, user);
+  d();
+})();
