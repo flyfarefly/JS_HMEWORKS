d77e8f23e09ee44fd587cd5bb5fb985d1f827ec4
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-06-04 16:53:11 +0300

    HW_68 first init

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 // eslint-disable-next-line no-unused-expressions,quotes
 `use strict`;
+(function () {
+  const createArrayIterator = function (arr) {
+    let index = 0;
+
+    return {
+      next() {
+        if (index < arr.length) {
+          // eslint-disable-next-line no-plusplus
+          return { value: arr[index++], done: false };
+        }
+        return { done: true };
+      },
+    };
+  };
+
+  // Приклад використання:
+  const myArray = [1, 2, 3, 4];
+
+  const iterator = createArrayIterator(myArray);
+
+  console.log(iterator.next()); // { value: 1, done: false }
+  console.log(iterator.next()); // { value: 2, done: false }
+  console.log(iterator.next()); // { value: 3, done: false }
+  console.log(iterator.next()); // { value: 4, done: false }
+  console.log(iterator.next()); // { done: true }
+})();
