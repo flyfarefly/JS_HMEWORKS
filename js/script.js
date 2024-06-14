ea7a267fd6bace230322c9755d7ff8bb3baae212
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-06-14 23:27:52 +0300

    HW_71 Start

--- js/script.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line no-unused-expressions,quotes
-`use strict`;
---
+++ src/scripts/todoList/constants.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DB_KEY = 'todo-items';
