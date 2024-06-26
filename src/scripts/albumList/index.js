4697e24e7f9c6e197102d78782899fb5b1d9ef88
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-06-26 16:27:30 +0300

    HW_76 Dead fish album link

--- src/scripts/albumList/index.js
+++ src/scripts/albumList/index.js
@@ -1,2 +1,20 @@
-// eslint-disable-next-line no-unused-expressions,quotes
-`use strict`;
+fetch('https://jsonplaceholder.typicode.com/albums')
+  .then((response) => response.json())
+  .then((albums) => {
+    // Створення списку елементів
+    const ul = document.createElement('ul');
+    albums.forEach((album) => {
+      const li = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = 'album1.html'; // Замініть на відповідний URL
+      console.log(link);
+      link.textContent = album.title;
+
+      li.textContent = album.title;
+      li.appendChild(link);
+      ul.appendChild(li);
+    });
+    // Додавання списку на сторінку
+    document.body.appendChild(ul);
+  })
+  .catch((error) => console.error('Помилка отримання даних:', error));
