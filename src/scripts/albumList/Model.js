90d790cb07ec6513c0764a3cdcdfe0c44defbca2
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-06-27 19:10:02 +0300

    HW_76 10 percent async await

--- src/scripts/albumList/Model.js
+++ src/scripts/albumList/Model.js
@@ -1,25 +1,26 @@
-fetch('https://jsonplaceholder.typicode.com/photos')
-  .then((response) => response.json())
-  .then((photos) => {
-    // Створення списку елементів
-    const div = document.getElementById('album-list');
-
-    photos.forEach((albumId) => {
-      // Створення елементів списку
-
-      const div = document.createElement('div');
-      const img = document.createElement('img');
-      const a = document.createElement('a');
-
-      img.src = albumId.thumbnailUrl;
-      img.alt = `Обкладинка альбому ${albumId.id}`;
-      a.href = albumId.url;
-      a.textContent = albumId.title;
-
-      div.appendChild(img);
-      div.appendChild(a);
-    });
-    // Додавання списку на сторінку
-    document.body.appendChild(div);
-  })
-  .catch((error) => console.error('Помилка отримання даних:', error));
+// fetch('https://jsonplaceholder.typicode.com/photos')
+//   .then((response) => response.json())
+//   .then((photos) => {
+//     // Створення списку елементів
+//     const ul = document.getElementById('album-list');
+//
+//     photos.forEach((albumId) => {
+//       // Створення елементів списку
+//
+//       const li = document.createElement('li');
+//       const img = document.createElement('img');
+//       const a = document.createElement('a');
+//
+//       img.src = albumId.thumbnailUrl;
+//       img.alt = `Обкладинка альбому ${albumId.id}`;
+//       a.href = albumId.url;
+//       a.textContent = albumId.title;
+//
+//       li.appendChild(img);
+//       li.appendChild(a);
+//       ul.appendChild(li);
+//     });
+//     // Додавання списку на сторінку
+//     document.body.appendChild(ul);
+//   })
+//   .catch((error) => console.error('Помилка отримання даних:', error));
--- src/scripts/albumList/index.js
+++ src/scripts/albumList/index.js
@@ -17,3 +17,25 @@
 //     document.body.appendChild(ul);
 //   })
 //   .catch((error) => console.error('Помилка отримання даних:', error));
+
+const getListAlbums = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/albums/');
+  const listAlbums = await response.json();
+  console.log(listAlbums);
+  const ul = document.createElement('ul');
+  listAlbums.forEach((album) => {
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+
+    link.href = 'album1.html'; // Замініть на відповідний URL
+    link.textContent = album.title;
+    li.textContent = album.id;
+    li.id = album.id;
+
+    li.appendChild(link);
+    ul.appendChild(li);
+  });
+  // Додавання списку на сторінку
+  document.body.appendChild(ul);
+};
+getListAlbums();
