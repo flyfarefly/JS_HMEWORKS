a179f8fdd36d8c4f7daa1df8df8734e801c677ac
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-06-27 15:46:39 +0300

    HW_76 10 percent

--- src/scripts/albumList/index.js
+++ src/scripts/albumList/index.js
@@ -1,20 +1,19 @@
-fetch('https://jsonplaceholder.typicode.com/albums')
-  .then((response) => response.json())
-  .then((albums) => {
-    // Створення списку елементів
-    const ul = document.createElement('ul');
-    albums.forEach((album) => {
-      const li = document.createElement('li');
-      const link = document.createElement('a');
-      link.href = 'album1.html'; // Замініть на відповідний URL
-      console.log(link);
-      link.textContent = album.title;
-
-      li.textContent = album.title;
-      li.appendChild(link);
-      ul.appendChild(li);
-    });
-    // Додавання списку на сторінку
-    document.body.appendChild(ul);
-  })
-  .catch((error) => console.error('Помилка отримання даних:', error));
+// fetch('https://jsonplaceholder.typicode.com/albums')
+//   .then((response) => response.json())
+//   .then((albums) => {
+//     // Створення списку елементів
+//     const ul = document.createElement('ul');
+//     albums.forEach((album) => {
+//       const li = document.createElement('li');
+//       const link = document.createElement('a');
+//
+//       link.href = 'album1.html'; // Замініть на відповідний URL
+//       link.textContent = album.title;
+//       li.textContent = album.title;
+//       li.appendChild(link);
+//       ul.appendChild(li);
+//     });
+//     // Додавання списку на сторінку
+//     document.body.appendChild(ul);
+//   })
+//   .catch((error) => console.error('Помилка отримання даних:', error));
