b50d7209ea5c80a6949336f1ccfc5c50b4cc9668
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-07-11 13:16:26 +0300

    HW_59 Complete full

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,14 @@ module.exports = {
     ecmaVersion: "latest",
   },
   rules: {
+    "no-restricted-syntax": "off",
+    "no-param-reassign": "off",
+    "func-names": "off",
     "no-console": "off",
     "wrap-iife": ["error", "any"], // Ensures IIFEs are wrapped in parentheses
-    "no-extra-parens": ["error", "functions"],
+    "no-extra-parens": ["error", "functions"], // Disallows unnecessary parentheses around functions
+    "no-unused-expressions": "off", // Errors on unused expressions which might be misused IIFEs
+    "quotes": ["off", "single"],
   },
   ignorePatterns: [".eslintrc.js"],
 };
--- js/solution.js
+++ js/solution.js
@@ -1,2 +1,20 @@
-// eslint-disable-next-line no-unused-expressions,quotes
-// `use strict`;
+`use strict`;
+
+(function () {
+  const listItems = document.querySelectorAll('li'); // Вибираємо всі елементи li
+
+  // Виводимо кожен елемент списку в консоль
+  for (const item of listItems) {
+    console.log(item.textContent);
+  }
+
+  // Виводимо загальну кількість елементів в консоль
+  console.log(`Загальна кількість елементів: ${listItems.length}`);
+
+  // Витягуємо текст із тегів li та записуємо в масив
+  const textArray = Array.from(listItems, (item) => item.textContent.trim());
+
+  // Виводимо масив текстів у консоль
+  console.log('Масив текстів з елементів li:');
+  console.log(textArray);
+})();
