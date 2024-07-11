92c566e1512083867bc64e316743526e73b7d5b7
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-07-11 16:43:51 +0300

    HW_65 files add descriptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,32 @@ module.exports = {
     ecmaVersion: "latest",
   },
   rules: {
-    "no-console": "off",
-    "wrap-iife": ["error", "any"], // Ensures IIFEs are wrapped in parentheses
-    "no-extra-parens": ["error", "functions"],
+    'no-continue': 'off',
+    'no-throw-literal': 'off',
+    'no-console': 'off',
+    'wrap-iife': ['error', 'any'], // Ensures IIFEs are wrapped in parentheses
+    'no-extra-parens': ['error', 'functions'],
+    'no-underscore-dangle': 'off',
+    'no-plusplus': 'off',
+    'comma-dangle': 'off',
+    'curly': 'off',
+    'no-alert': 'error',
+    'operator-linebreak': 'error',
+    'no-undefined': 'error',
+    'nonblock-statement-body-position': 'off',
+    'accessor-pairs': 'off',
+    'no-multi-spaces': [
+      'error',
+      {
+        ignoreEOLComments: true,
+        exceptions: {
+          AssignmentExpression: true,
+          ArrowFunctionExpression: true,
+          CallExpression: true,
+          VariableDeclarator: true,
+        },
+      },
+    ],
   },
   ignorePatterns: [".eslintrc.js"],
 };
