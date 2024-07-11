d90df7e67c609d18942ce0b37fe005d1ff151758
flyfarefly <103898982+flyfarefly@users.noreply.github.com>
2024-07-11 16:41:16 +0300

    HW_66 files add

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,48 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "airbnb-base",
+  extends: 'airbnb-base',
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
   rules: {
-    "no-console": "off",
-    "wrap-iife": ["error", "any"], // Ensures IIFEs are wrapped in parentheses
-    "no-extra-parens": ["error", "functions"],
+    'no-continue': 'off',
+    'no-throw-literal': 'off',
+    'no-console': 'off',
+    'wrap-iife': ['error', 'any'], // Ensures IIFEs are wrapped in parentheses
+    'no-extra-parens': ['error', 'functions'],
+    'no-underscore-dangle': 'off',
+    'no-plusplus': 'off',
+    'comma-dangle': 'off',
+    curly: 'off',
+    'no-alert': 'error',
+    'operator-linebreak': 'error',
+    'no-undefined': 'error',
+    'nonblock-statement-body-position': 'off',
+    'accessor-pairs': 'off',
+    'no-multi-spaces': [
+      'error',
+      {
+        ignoreEOLComments: true,
+          exceptions: {
+          AssignmentExpression: true,
+          ArrowFunctionExpression: true,
+          CallExpression: true,
+          VariableDeclarator: true,
+        },
+      },
+    ],
   },
-  ignorePatterns: [".eslintrc.js"],
+  ignorePatterns: ['.eslintrc.js'],
 };
--- js/script.js
+++ js/script.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line no-unused-expressions,quotes
 `use strict`;
